7b6646e96d2e30de310e85ca38ece4beadda3517
liwenqiang <wenqiang.li@mkcorp.com>
2018-09-13 15:35:50 +0800

    [+]  文件格式限制及readme说明

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,10 +1,6 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const fs = require('fs');
-const awaitWriteStream = require('await-stream-ready').write;
-const path = require('path');
-const { errorEvent } = require('../util/errorEvent');
 
 class HomeController extends Controller {
   async index() {
@@ -14,17 +10,8 @@ class HomeController extends Controller {
 
   async upload() {
     const { ctx } = this;
-    const srcStream = await ctx.getFileStream();
-    if (!/\.docx$|\.doc$/.test(srcStream.filename)) {
-      errorEvent('fileType');
-    }
 
-    // 保存文件到改路径
-    const file = path.join(path.resolve('./app/file/'), srcStream.filename);
-    const stream = srcStream.pipe(fs.createWriteStream(file));
-    await awaitWriteStream(stream);
-
-    const parseWordDocGetJson = await ctx.service.index.parseWordDoc(file);
+    const parseWordDocGetJson = await ctx.service.index.parseWordDoc();
 
     ctx.body = {
       data: parseWordDocGetJson,
--- app/service/index.js
+++ app/service/index.js
@@ -1,11 +1,30 @@
 'use strict';
 
 const Service = require('egg').Service;
-const parseWordDocGetJson = require('./parseWordDoc').getJson;
+const fs = require('fs');
+const path = require('path');
+const awaitWriteStream = require('await-stream-ready').write;
+
+const { errorEvent } = require('../util/errorEvent');
+const { getJson: parseWordDocGetJson/* , getJsonPath: parseWordDocGetJsonFile */ } = require('./parseWordDoc');
 
 class IndexService extends Service {
-  parseWordDoc(fileName) {
-    return parseWordDocGetJson(fileName);
+  async parseWordDoc() {
+    const { ctx } = this;
+    const srcStream = await ctx.getFileStream();
+    if (!/\.docx$|\.doc$/.test(srcStream.filename)) {
+      errorEvent('fileType');
+    }
+    // 保存文件到改路径
+    const file = path.join(path.resolve('./app/file/'), srcStream.filename);
+    const stream = srcStream.pipe(fs.createWriteStream(file));
+    await awaitWriteStream(stream);
+
+    // getFile
+    // return parseWordDocGetJsonFile(file)
+
+    // getJson
+    return parseWordDocGetJson(file);
   }
 }
 
